4129ea16606bf3a19a7398c951c335c39e5f0320
Sewlz <tnam7232@gmail.com>
2023-03-27 22:42:03 +0700

    connect to database

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const app = require("./app");
 const dotenv = require("dotenv");
-// const sql = require('mssql')
+const sql = require("mssql");
+
 dotenv.config({
   path: "./config.env",
 });
+const dbConfig = require("./database/dbconfig");
+
+const appPool = new sql.ConnectionPool(dbConfig.sqlConfig);
+appPool
+  .connect()
+  .then((pool) => {
+    console.log("SQL Connected!");
+    dbConfig.db.pool = pool;
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
