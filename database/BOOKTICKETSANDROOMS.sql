cf38a0b2aa8304a2fa85cc413a461f6339206bd2
your name <abc@gmail.com>
2023-04-20 20:07:33 +0700

    upload data to database

--- database/BOOKTICKETSANDROOMS.sql
+++ database/BOOKTICKETSANDROOMS.sql
@@ -13,6 +13,11 @@ CREATE TABLE ROLES(
 
 GO
 
+INSERT INTO ROLES(Name) VALUES
+('user'), ('hotelManager'), ('flightManager'), ('admin')
+
+GO
+
 CREATE TABLE USERS(
 	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	Username VARCHAR(50) NOT NULL UNIQUE,
@@ -28,16 +33,31 @@ CREATE TABLE USERS(
 
 GO
 
+INSERT INTO USERS(Username, Password, Name, Email, PhoneNumber, Birthday, RoleId) VALUES
+('Admin', '123456', 'Nguyen Van A', 'Admin01@gmail.com', '0983923839', '4/23/1995', 1),
+('HotelManager', '123456', 'Nguyen Van B', 'HotelManager01@gmail.com', '0937463854', '9/12/1994', 2),
+('FlightManager', '123456', 'Nguyen Van C', 'FlightManager01@gmail.com', '09384756122', '1/1/1996', 3),
+('User', '123456', 'Nguyen Van D', 'User01@gmail.com', '0974564123', '2/3/2000', 4)
+
+GO
+
 CREATE TABLE HOTELS(
 	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	Name NVARCHAR(100) NOT NULL, -- Tên khách sạn
 	PhoneNumber NVARCHAR(20) NULL, -- Số điện thoại liên hệ
-	Email VARCHAR(100) NULL, -- Email liên hệ
+	Email VARCHAR(100) NULL UNIQUE, -- Email liên hệ
 	Address NVARCHAR(200) NOT NULL, -- Địa chỉ khách sạn
 	City NVARCHAR(200) NOT NULL,
 	EmtyRooms INT DEFAULT 0 NULL,
 	CreateAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 )
+
+GO
+
+INSERT INTO HOTELS(Name, PhoneNumber, Email, Address, City) VALUES
+('Hotel 01', '0928374832', 'Hotel01@gmail.com', 'HCM', 'HCM'),
+('Hotel 02', '0823465789', 'Hotel02@gmail.com', 'HN', 'HN'),
+('Hotel 03', '0283728145', 'Hotel03@gmail.com', 'DN', 'DN')
 
 GO
 
@@ -53,6 +73,34 @@ CREATE TABLE ROOMS(
 
 GO
 
+--insert hotel01
+INSERT INTO ROOMS(Name, Price, HotelId) VALUES
+('H01P01', 300, 1),
+('H01P02', 200, 1),
+('H01P03', 300, 1),
+('H01P04', 800, 1),
+('H01P05', 500, 1)
+GO
+
+--insert hotel02
+INSERT INTO ROOMS(Name, Price, HotelId) VALUES
+('H02P01', 1000, 2),
+('H02P02', 900, 2),
+('H02P03', 550, 2),
+('H02P04', 1100, 2),
+('H02P05', 800, 2)
+GO
+
+--insert hotel03
+INSERT INTO ROOMS(Name, Price, HotelId) VALUES
+('H03P01', 1000, 3),
+('H03P02', 1000, 3),
+('H03P03', 1000, 3),
+('H03P04', 1000, 3),
+('H03P05', 1000, 3)
+
+GO
+
 CREATE TABLE AIRPORTS(
 	Id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
 	Name NVARCHAR(100) NOT NULL, -- Tên sân bay
@@ -60,6 +108,13 @@ CREATE TABLE AIRPORTS(
 	PhoneNumber NVARCHAR(20) NULL, -- Số điện thoại liên hệ
 	CreateAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 )
+
+GO
+
+INSERT INTO AIRPORTS(Name, Address, PhoneNumber) VALUES
+('Tan Son Nhat', 'HCM', '0928743646'),
+('Noi Bai', 'HN', '0792832744')
+
 GO
 
 CREATE TABLE AIRLINECLASS(
@@ -68,6 +123,15 @@ CREATE TABLE AIRLINECLASS(
 	Price FLOAT NOT NULL, -- Giá ghế
 	CreateAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 )
+
+GO
+
+INSERT INTO AIRLINECLASS(Name, Price) VALUES
+('Pho thong', 1000),
+('Pho thong dac biet', 2000),
+('Thuong gia', 3000),
+('Hang nhat', 4000)
+
 GO
 
 CREATE TABLE FLIGHTS(
@@ -76,10 +140,20 @@ CREATE TABLE FLIGHTS(
 	FromLocation NVARCHAR(100) NOT NULL, -- Nơi đi
 	ToLocation NVARCHAR(100) NOT NULL, -- Nơi đến
 	DateOfDepartment DATETIME NOT NULL, -- Ngày đi
-	EmptySeat INT NOT NULL, -- Số lượng ghế trống 
+	EmptySeat INT NULL, -- Số lượng ghế trống 
 	AirportId INT CONSTRAINT FK_FLIGHTS_AIRPORTS FOREIGN KEY (AirportId) REFERENCES AIRPORTS(Id),
-	CreateAt DATETIME  NOT NULL/*DEFAULT CURRENT_TIMESTAMP*/,
-)																								
+	CreateAt DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+)	
+
+GO
+
+INSERT INTO FLIGHTS(Name, FromLocation, ToLocation, DateOfDepartment, AirportId) VALUES
+('Flight 01', 'HCM', 'HN', '5/20/2023', '1'),
+('Flight 02', 'HCM', 'BMT', '5/23/2023', '1'),
+('Flight 03', 'HN', 'HN', '5/11/2023', '2'),
+('Flight 04', 'HN', 'BMT', '5/2/2023', '2')
+
+
 GO
 
 CREATE TABLE SEATS(
@@ -90,6 +164,66 @@ CREATE TABLE SEATS(
 	FlightId INT  FOREIGN KEY (FlightId) REFERENCES FLIGHTS(Id) NOT NULL,
 	CreateAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 )
+
+GO
+
+--insert flight01
+INSERT INTO SEATS(name, AirlineClassId, FlightId) VALUES
+('S01', 1, 1),
+('S02', 1, 1),
+('S03', 1, 1),
+('S04', 1, 1),
+('S05', 2, 1),
+('S06', 2, 1),
+('S07', 2, 1),
+('S08', 3, 1),
+('S09', 3, 1),
+('S10', 4, 1)
+GO
+
+--insert flight02
+INSERT INTO SEATS(name, AirlineClassId, FlightId) VALUES
+('S01', 1, 2),
+('S02', 1, 2),
+('S03', 1, 2),
+('S04', 1, 2),
+('S05', 2, 2),
+('S06', 2, 2),
+('S07', 2, 2),
+('S08', 3, 2),
+('S09', 3, 2),
+('S10', 4, 2)
+GO
+
+--insert flight03
+INSERT INTO SEATS(name, AirlineClassId, FlightId) VALUES
+('S01', 1, 3),
+('S02', 1, 3),
+('S03', 1, 3),
+('S04', 1, 3),
+('S05', 2, 3),
+('S06', 2, 3),
+('S07', 2, 3),
+('S08', 3, 3),
+('S09', 3, 3),
+('S10', 4, 3)
+GO
+
+--insert flight04
+INSERT INTO SEATS(name, AirlineClassId, FlightId) VALUES
+('S01', 1, 4),
+('S02', 1, 4),
+('S03', 1, 4),
+('S04', 1, 4),
+('S05', 2, 4),
+('S06', 2, 4),
+('S07', 2, 4),
+('S08', 3, 4),
+('S09', 3, 4),
+('S10', 4, 4)
+GO
+
+
 GO
 
 CREATE TABLE BOOKINGROOMS(
@@ -105,6 +239,7 @@ CREATE TABLE BOOKINGROOMS(
 	CheckOutDate DATE NULL,
 	Status NVARCHAR(50) NOT NULL, 
 )
+
 GO
 
 CREATE TABLE BOOKINGFLIGHTS(
@@ -115,6 +250,7 @@ CREATE TABLE BOOKINGFLIGHTS(
 	Status NVARCHAR(50) NOT NULL,
 	CreateAt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
 )
+
 GO
 
 --AUTO UPDATE SEATS
@@ -156,14 +292,14 @@ GO
 -- END;
 --GO
 
-CREATE TRIGGER roomStatus ON BOOKINGROOMS AFTER INSERT AS 
-BEGIN
-	UPDATE ROOMS
-	SET Status = 'FALSE'
-	From ROOMS
-	JOIN inserted ON ROOMS.Id = inserted.RoomId
-END;
-GO
+--CREATE TRIGGER roomStatus ON BOOKINGROOMS AFTER INSERT AS 
+--BEGIN
+--	UPDATE ROOMS
+--	SET Status = 'FALSE'
+--	From ROOMS
+--	JOIN inserted ON ROOMS.Id = inserted.RoomId
+--END;
+--GO
 
 
 /*ROOMS*/
@@ -190,16 +326,16 @@ GO
 -- END;
 -- GO
 
-CREATE TRIGGER trg_UpdateCheckOutDate
-ON BOOKINGROOMS
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    UPDATE BOOKINGROOMS
-    SET CheckOutDate = DATEADD(day, NumberDay, CheckInDate)
-    FROM BOOKINGROOMS
-    WHERE BOOKINGROOMS.Id IN (SELECT Id FROM inserted)
-END;
+--CREATE TRIGGER trg_UpdateCheckOutDate
+--ON BOOKINGROOMS
+--AFTER INSERT, UPDATE
+--AS
+--BEGIN
+--    UPDATE BOOKINGROOMS
+--    SET CheckOutDate = DATEADD(day, NumberDay, CheckInDate)
+--    FROM BOOKINGROOMS
+--    WHERE BOOKINGROOMS.Id IN (SELECT Id FROM inserted)
+--END;
 
 
 --drop database BookingTicketsAndRooms
@@ -256,24 +392,24 @@ SELECT * FROM SEATS WHERE Status = 'TRUE' AND Id = 4
 
 --ADD ALL SEAT BY FLIGHT ID
 
---DECLARE @step INT = 1,
---@seats INT = (SELECT EmptySeat from Flights where id = 2)
---WHILE @step <= @seats
---begin 
---	if(@step <= 10)
---		begin
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('A'+CONVERT(varchar,@step),2,'TRUE',2)
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('B'+CONVERT(varchar,@step),2,'TRUE',2)
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('C'+CONVERT(varchar,@step),2,'TRUE',2)
---		end
---	else
---		begin
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('A'+CONVERT(varchar,@step),12,'TRUE',2)
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('B'+CONVERT(varchar,@step),12,'TRUE',2)
---			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('C'+CONVERT(varchar,@step),12,'TRUE',2)
---		end
---set @step = @step +1
---end
+DECLARE @step INT = 1,
+@seats INT = (SELECT EmptySeat from Flights where id = 2)
+WHILE @step <= @seats
+begin 
+	if(@step <= 10)
+		begin
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('A'+CONVERT(varchar,@step),2,'TRUE',2)
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('B'+CONVERT(varchar,@step),2,'TRUE',2)
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('C'+CONVERT(varchar,@step),2,'TRUE',2)
+		end
+	else
+		begin
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('A'+CONVERT(varchar,@step),12,'TRUE',2)
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('B'+CONVERT(varchar,@step),12,'TRUE',2)
+			Insert into SEATS (Name, AirlineClassId, Status,FlightId) values ('C'+CONVERT(varchar,@step),12,'TRUE',2)
+		end
+set @step = @step +1
+end
 
 
 --SELECT * FROM SEATS
